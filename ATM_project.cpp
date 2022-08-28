176d18bdb62bba533598b2845e85fe7f4b5be94b
SakRoi <roininensaku@gmail.com>
2022-08-28 20:32:00 +0300

    added basic code for the withdraw function, moved the cout code that displayed the balance out of moneyUser function and into the main. Changed the usage of moneyUser to work as a check for peole to exit or contiune using the script. Changed moneyUser to returnMenu to show this change of purpose.

--- ATM_project.cpp
+++ ATM_project.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <stdlib.h>
 
-//forward declaration for userMoney function
-double userMoney(double balance);
+//forward declaration for returnMenu function
+void returnMenu();
 
 int main(){
-    double balance = 100;
     int switchSelection = 0;
+    double balance = 0;
     double depositBalance = 0;
+    double withdrawBalance = 0;
 
     while (1){
         std::cout << "This is a bank ATM\n" << "---------------------\n" << "please make a choice: \n" << "1. Balance \n" << "2. Deposit \n" << "3. Withdraw \n" << "4. Exit\n" << "---------------------" << std::endl;
         std::cin >> switchSelection;
         switch (switchSelection){
+
+        //balance
         case 1:
-        userMoney(balance);
+        std::cout << "the accounts balance is: " << balance;
+        returnMenu();
         break;
+
+        //deposit
         case 2:
         //this will give an opinion to add to the account's balance
         std::cout << "Please insert the amount of money you will be depositing: \n";
         std::cin >> depositBalance;
         std::cout << "\n";
+        /**this is here to check for negative value given, if one is given it will automatically move
+         * the user to the main menu **/
         if (depositBalance < 0){
             std::cout << "deposit amount is negative. \nReturning to menu.\n";
         }
@@ -28,26 +36,39 @@ int main(){
         balance = balance + depositBalance;
         }
         break;
+
+        //withdraw
         case 3:
         //this will give an opinion to remove from the account's balance
-        std::cout << "withdraw" << std::endl;
+        std::cout << "Insert the amount of money you will be withdrawing: \n";
+        std::cin >> withdrawBalance;
+        std::cout << "\n";
+        if (withdrawBalance < 0){ 
+            //this is here to check if the given value is negative and convert it back to a positive one.
+            withdrawBalance = withdrawBalance * -1;
+            balance = balance - withdrawBalance;
+        }
+        else {
+        balance = balance - withdrawBalance;
+        }
         break;
+
         case 4:
         return 0;
         break;
+
         default:
         std::cout << "invalid choice" << std::endl;
         }
     }
 }
-double userMoney(double balance) {
+void returnMenu() {
     int contSelection = 0;
     //this will display the balance of the account
-    std::cout << "the accounts balance is: " << balance << "\nReturn to menu?\n" << "1. Menu\n" << "2. Exit" << std::endl;
+    std::cout << "\nReturn to menu?\n" << "1. Menu\n" << "2. Exit" << std::endl;
     std::cin >> contSelection;
         switch (contSelection){
             case 1:
-            return balance;
             break;
             case 2:
             //using exit here because using return 0; doesn't end the program early.
@@ -55,6 +76,5 @@ double userMoney(double balance) {
             break;
             default:
             std::cout << "invalid option, returning to menu" << std::endl;
-            return balance;
             }
 }
